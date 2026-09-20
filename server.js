47ec0677cbab7d04454d638823ccbe28d0071331
rony1030 <ronyabellor@gmail.com>
2026-09-20 11:25:43 -0400

    fix(crm): optimize evidence image compression on upload and raise server payload body limit to 50mb

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/favicon.ico', (req, res) => res.sendFile(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use((req, res, next) => {
   req.cookies = {};
   const rc = req.headers.cookie;
